f8fadf9f429941fa3c27125764aec9736b82c32c
Mekarosi <mekarosi_assa@yahoo.com>
2020-04-01 19:13:12 +0100

    Add state properties to create events

--- client/src/events/CreateEvent.js
+++ client/src/events/CreateEvent.js
@@ -1,7 +1,27 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
-export default class CreateEvent extends Component {
+class CreateEvent extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      event_description: '',
+      event_priority: '',
+      event_completed: false,
+      event_startDate: '',
+      event_endDate: '',
+      calendar_description: ''
+    };
+  }
+
   render() {
-    return <div></div>;
+    return (
+      <div>
+        <h2>Create an Event</h2>
+      </div>
+    );
   }
 }
+
+export default CreateEvent;

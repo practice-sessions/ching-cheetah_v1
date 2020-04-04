e637da2a110ec3114bb381168506f7483e4d1bdd
Mekarosi <mekarosi_assa@yahoo.com>
2020-04-04 08:42:41 +0100

    Create component to delete event and use handler and axios

--- client/src/events/DeleteEvent.js
+++ client/src/events/DeleteEvent.js
@@ -1,16 +1,129 @@
 import React, { Component } from 'react';
-import axios from axios
+import axios from 'axios';
 
- class DeleteEvent extends Component {
+class DeleteEvent extends Component {
+  constructor(props) {
+    super(props);
 
-   
+    this.state = {
+      event_decription: '',
+      calendar_description: '',
+      event_priority: '',
+      event_completed: false
+    };
+  }
+
+  onChangeCalendarDescription = e => {
+    this.setState({ event_decription: e.target.value });
+  };
+
+  onChangeCalendarDescription = e => {
+    this.setState({ calendar_description: e.target.value });
+  };
+
+  onChangeEventPriority = e => {
+    this.setState({ event_priority: e.target.value });
+  };
+
+  onSubmit = e => {
+    e.preventDefault();
+
+    const obj = {
+      event_decription: this.state.event_decription,
+      calendar_description: this.state.calendar_description,
+      event_priority: this.state.event_priority
+    };
+
+    console.log(obj);
+
+    axios
+      .delete(
+        'http://api/events/delete/' +
+          this.props.match.event_id +
+          this.props.match.calendar_id,
+        obj
+      )
+      .then(res => console.log(res.data));
+    this.props.history.push('/');
+  };
+
   render() {
     return (
       <div>
-        <h3>Delete Events</h3>
+        <h3 align='center'>Delete Event</h3>
+        <form onSubmit={this.onSubmit}>
+          <div className='form-group'>
+            <label>Description:</label>
+            <imput
+              type='text'
+              className='form-control'
+              value={this.state.event_decription}
+              onChange={this.onChangeEventDecription}
+            />
+          </div>
+
+          <div className='form-group'>
+            <label>Calendar Date:</label>
+            <imput
+              type='text'
+              className='form-control'
+              value={this.state.calendar_description}
+              onChange={this.onChangeCalendarDescription}
+            />
+          </div>
+
+          <div className='form-group'>
+            <div className='form-check form-check-inline'>
+              <imput
+                className='form-check-imput'
+                type='radio'
+                name='priorityOptions'
+                id='priorityLow'
+                value='Low'
+                checked={this.state.event_priority === 'Low'}
+                onChange={this.onChangeEventPriority}
+              />
+              <label className='form-check-label'>Low</label>
+            </div>
+
+            <div className='form-check form-check-inline'>
+              <imput
+                className='form-check-input'
+                type='radio'
+                name='prioriyOptions'
+                id='priorityMedium'
+                value='Medium'
+                checked={this.state.event_priority === 'Medium'}
+                onChange={this.onChangeEventPriority}
+              />
+              <label className='form-check-label'>Medium</label>
+            </div>
+            <div className='form-check form-check-inline'>
+              <imput
+                className='form-check-input'
+                type='radio'
+                name='priorityOptions'
+                id='priorityHigh'
+                value='High'
+                checked={this.state.event_priority === 'High'}
+                onChange={this.onChangeEventPriority}
+              />
+              <label className='form-check-label'>High</label>
+            </div>
+            <br />
+
+            <div className='form-group'>
+              <input
+                type='submit'
+                value='Delete Event'
+                className='btn btn-danger'
+              />
+            </div>
+          </div>
+        </form>
       </div>
     );
   }
 }
 
-export default DeleteEvent
+export default DeleteEvent;
